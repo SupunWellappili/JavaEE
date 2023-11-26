423255e003228722e82902be56fe461c6b4a19c8
SupunWellappili <supunchanada1814@gmail.com>
2023-11-26 09:46:44 +0530

    Create Response

--- JavaEE/src/HelloServlet.java
+++ JavaEE/src/HelloServlet.java
@@ -1,7 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "MyServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
-    //Web Application
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+        System.out.println(" Request Came");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Sended");
 
-
+    }
 }
